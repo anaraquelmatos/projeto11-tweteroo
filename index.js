22cfea1d9e4d260bea9ec997c30b6f37187ff625
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-04-24 00:49:41 -0300

    POST /tweets

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 const usuario = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
@@ -15,6 +16,18 @@ app.post("/sign-up", (req, res) => {
         console.log(usuario);
     } else {
         res.send("Confira as informações repassadas!");
+    }
+})
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    if (username !== null && tweet !== null) {
+        const infos = usuario.find(elemento => elemento.username === username);
+        tweets.push({ username: username, avatar: infos.avatar, tweet: tweet});
+        res.status(200).send("OK");
+        console.log(tweets);
+    } else {
+        res.send("Confira as informações colocadas!");
     }
 })
 
