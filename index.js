c02f4d3544c8b22d01bf23bb9db02b02bda97f30
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-04-24 00:48:10 -0300

    POST /sign-up

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
 app.use(cors());
 
+const usuario = [];
 
-
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    if (username !== null && avatar !== null) {
+        usuario.push({ username: username, avatar: avatar });
+        res.status(200).send("OK");
+        console.log(usuario);
+    } else {
+        res.send("Confira as informações repassadas!");
+    }
+})
 
 app.listen(5000);
